e0f29966c603fa8898cef398f3f5863ad2c98a64
Ame314 <amelie.grobrosero@gmail.com>
2023-12-13 08:53:29 -0500

    Agregar archivos de proyecto.

---
+++ Tarea_16_StringGrob/frmLecturaArchivos.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Tarea_16_StringGrob
+{
+    partial class frmLecturaArchivos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtArchivos = new System.Windows.Forms.TextBox();
+            this.btnCargarArchivo = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnGenerarNom = new System.Windows.Forms.Button();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCargarArchivo);
+            this.groupBox1.Controls.Add(this.txtArchivos);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(746, 91);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seleccione el archivo de texto";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre del archivo";
+            // 
+            // txtArchivos
+            // 
+            this.txtArchivos.Location = new System.Drawing.Point(24, 41);
+            this.txtArchivos.Name = "txtArchivos";
+            this.txtArchivos.Size = new System.Drawing.Size(610, 20);
+            this.txtArchivos.TabIndex = 1;
+            // 
+            // btnCargarArchivo
+            // 
+            this.btnCargarArchivo.Location = new System.Drawing.Point(640, 39);
+            this.btnCargarArchivo.Name = "btnCargarArchivo";
+            this.btnCargarArchivo.Size = new System.Drawing.Size(91, 23);
+            this.btnCargarArchivo.TabIndex = 2;
+            this.btnCargarArchivo.Text = "Cargar Archivo";
+            this.btnCargarArchivo.UseVisualStyleBackColor = true;
+            this.btnCargarArchivo.Click += new System.EventHandler(this.btnCargarArchivo_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(666, 359);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(77, 22);
+            this.btnCerrar.TabIndex = 1;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // btnGenerarNom
+            // 
+            this.btnGenerarNom.Location = new System.Drawing.Point(27, 135);
+            this.btnGenerarNom.Name = "btnGenerarNom";
+            this.btnGenerarNom.Size = new System.Drawing.Size(105, 23);
+            this.btnGenerarNom.TabIndex = 2;
+            this.btnGenerarNom.Text = "generar nombre";
+            this.btnGenerarNom.UseVisualStyleBackColor = true;
+            this.btnGenerarNom.Click += new System.EventHandler(this.btnGenerarNom_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(171, 140);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(179, 13);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "EL NOMBRE SELECCIONADO ES :";
+            // 
+            // frmLecturaArchivos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.btnGenerarNom);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmLecturaArchivos";
+            this.Text = "frmLecturaArchivos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCargarArchivo;
+        private System.Windows.Forms.TextBox txtArchivos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnGenerarNom;
+        private System.Windows.Forms.Label lblNombre;
+    }
+}
---
+++ Tarea_16_StringGrob/frmLecturaArchivos.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO;
+using System.Drawing.Text;
+
+namespace Tarea_16_StringGrob
+{
+    public partial class frmLecturaArchivos : Form
+    {
+        const int MAX = 1000;
+        string[] arrayNombres;
+        int totalElementos;
+        public frmLecturaArchivos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargarArchivo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = Environment.SpecialFolder.MyDocuments.ToString(),
+                Title = "Seleccione el archivo de texto",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+           
+
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    this.txtArchivos.Text = openFileDialog1.FileName;
+                    this.LeerArchivoTexto(this.txtArchivos.Text);
+                }
+            }
+        private void LeerArchivoTexto(string nombreArchivo)
+        {
+            String line;
+            arrayNombres = new string[MAX];
+            int cont = 0, totalElementos = 0;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader(nombreArchivo, Encoding.UTF8);
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the line to console window
+                    //Console.WriteLine(line); enviar los elementos al vector
+                   if (cont < MAX)
+                    {
+                        arrayNombres[cont] = line;
+                        totalElementos++;
+                    }
+                    line = sr.ReadLine();
+                    cont++;
+
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+           
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGenerarNom_Click(object sender, EventArgs e)
+        {
+            this.lblNombre.Text = arrayNombres[1];
+        }
+    }
+}
